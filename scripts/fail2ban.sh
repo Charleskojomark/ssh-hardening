c9d178833366da287c0f2284b7fe040d7c68ae4b
Charleskojomark <markcharleskojo@gmail.com>
2025-12-19 14:06:43 +0100

    fix: refactor relative path in fail2ban script

--- scripts/fail2ban.sh
+++ scripts/fail2ban.sh
@@ -7,8 +7,11 @@ source scripts/logging.sh
 setup_failban() {
     log "Installing and configuring fail2ban..."
 
+    SCRIPT_DIR="$(dirname "$0")"
+    JAIL_LOCAL="$SCRIPT_DIR/configs/jail.local"
+
     sudo apt install -y fail2ban
-    cp /configs/jail.local /etc/fail2ban/jail.local
+    sudo cp "$JAIL_LOCAL" /etc/fail2ban/jail.local
     
     sudo systemctl enable fail2ban
     sudo systemctl restart fail2ban
